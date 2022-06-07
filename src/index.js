45f03b77d50b7d01dab57debaea37fcd875a5011
luizphelip3 <luiz_phelip3@hotmail.com>
2022-06-07 16:19:54 -0300

    feat: added simple post method, without using database

--- src/index.js
+++ src/index.js
@@ -1,5 +1,33 @@
-const express = require('express')
+const express = require("express");
+
+const { v4: uuidv4 } = require("uuid");
 
 const app = express();
 
-app.listen(3838)
+app.use(express.json());
+
+const costumers = [];
+
+/**
+ * cpf - string
+ * name - string
+ * id - uuid
+ * statement - []
+ */
+
+app.post("/account", (req, res) => {
+  const { cpf, name } = req.body;
+
+  const id = uuidv4();
+
+  costumers.push({
+    cpf,
+    name,
+    id,
+    statement: [],
+  });
+
+  return res.status(201).send(costumers);
+});
+
+app.listen(3333);
